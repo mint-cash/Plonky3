c7208be36077fa5ebcc8242dfbe58fdf9c4dd8c0
Junho Yeo <i@junho.io>
2024-09-02 17:53:33 +0900

    Pass `instrument` values as `str`

--- merkle-tree/src/merkle_tree.rs
+++ merkle-tree/src/merkle_tree.rs
@@ -33,8 +33,9 @@ impl<F: Clone + Send + Sync, W: Clone, M: Matrix<F>, const DIGEST_ELEMS: usize>
 {
     /// Matrix heights need not be powers of two. However, if the heights of two given matrices
     /// round up to the same power of two, they must be equal.
-    #[instrument(name = "build merkle tree", level = "debug", skip_all,
-                 fields(dimensions = alloc::format!("{:?}", leaves.iter().map(|l| l.dimensions()).collect::<Vec<_>>())))]
+    #[instrument(name = "build merkle tree", level = "debug", skip_all, fields(dimensions =
+        alloc::format!("{:?}", leaves.iter().map(|l| l.dimensions()).collect::<Vec<_>>()).as_str()
+    ))]
     pub fn new<P, PW, H, C>(h: &H, c: &C, leaves: Vec<M>) -> Self
     where
         P: PackedField<Scalar = F>,
